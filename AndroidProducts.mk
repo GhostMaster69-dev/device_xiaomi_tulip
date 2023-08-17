2c3d708f6681a7d678bea1230ab66cdc21a7d499
GhostMaster69-dev <rathore6375@gmail.com>
2023-08-18 01:12:04 +0530

    tulip: derp-ify
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tulip.mk
+    $(LOCAL_DIR)/derp_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_tulip-user \
-    lineage_tulip-userdebug \
-    lineage_tulip-eng
+    derp_tulip-user \
+    derp_tulip-userdebug \
+    derp_tulip-eng
